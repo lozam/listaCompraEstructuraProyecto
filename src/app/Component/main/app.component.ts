b46d0e3d86652fad968d80eb2269ba751585f9fb
lozam <loza_maicol@hotmail.com>
2017-06-16 03:29:38 +0200

    Subida 2 no funciona bootstrap

--- src/app/Component/main/app.component.ts
+++ src/app/Component/main/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import{Producto} from '../../classes/Producto'
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hola Maicol {{name}}</h1>
-  <h2>Mi primer componente.</h2>`,
+  templateUrl: 'app.component.html',
 })
-export class AppComponent  { name = 'LOZA'; }
+export class AppComponent  { 
+  name = 'LOZA'; 
+  producto : Producto = new Producto(
+    1, 'Papa', 1, 3, 'Papas para freir'
+  );
+}
